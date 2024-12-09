fc23cc8c40470bd57a2686bc152211eb54e838a5
Rodrigo Soares <soaresjw27@gmail.com>
2024-12-09 12:51:40 -0300

    Funcao Salvar funcionado

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -1,17 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author Adm
- */
 
 import java.sql.PreparedStatement;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 
@@ -22,13 +16,38 @@ public class ProdutosDAO {
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
-    public void cadastrarProduto (ProdutosDTO produto){
+    public boolean conectar(){
+        try {
         
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+    Class.forName("com.mysql.cj.jdbc.Driver");
+    conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/uc11","root","22095812");
+    return true;
+    
+        } catch (ClassNotFoundException | SQLException ex) {
+        System.out.println("Erro ao conectar" + ex.getMessage());
+        return false;
+        }
     }
+    
+    public int cadastrarProduto (ProdutosDTO produto){
+        //conn = new conectaDAO().connectDB();
+         int status;
+        
+        try {
+            prep = conn.prepareStatement("INSERT INTO produtos (nome, valor, status)VALUES(?,?,?)");
+            prep.setString(1,produto.getNome());
+            prep.setInt(2,produto.getValor());
+            prep.setString(3,produto.getStatus());
+            
+            status = prep.executeUpdate();
+            return status; 
+            
+            } catch (SQLException ex) {
+            System.out.println("“Não foi possível SALVAR os dados! Por favor, verifique valores digitados!”." + ex.getMessage());
+            return ex.getErrorCode();
+            }
+    }    
+    
     
     public ArrayList<ProdutosDTO> listarProdutos(){
         
--- LeiloesTDSat/src/conectaDAO.java
+++ LeiloesTDSat/src/conectaDAO.java
@@ -1,33 +1,31 @@
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
 
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author Adm
- */
 public class conectaDAO {
     
-    public Connection connectDB(){
-        Connection conn = null;
-        
+  
+    Connection conn;
+    PreparedStatement st;
+    ResultSet rs;
+    
+    public boolean conectar(){
         try {
         
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
-            
-        } catch (SQLException erro){
-            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
-        }
-        return conn;
-    }
+    Class.forName("com.mysql.cj.jdbc.Driver");
+    conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/uc11","root", "22095812");
+    return true;
     
+        } catch (ClassNotFoundException | SQLException ex) {
+        System.out.println("Erro ao conectar" + ex.getMessage());
+        return false;
+        }
+    }
 }
